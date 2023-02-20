01d05ec6efa5961f418aad3dee0c86161624037b
Raul Ibáñez <raulibaper@gmail.com>
2023-02-20 20:10:36 +0100

    removed commands

--- app.js
+++ app.js
@@ -14,21 +14,6 @@ const client = new Client({
 	partials: [Partials.Message, Partials.Channel, Partials.Reaction],
  });
 
-
-// Add command collection to client
-// https://discordjs.guide/creating-your-bot/command-handling.html#individual-command-files
-client.commands = new Collection();
-
-// Read command files
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	// Set a new item in the Collection
-	// With the key as the command name and the value as the exported module
-	client.commands.set(command.data.name, command);
-}
-
 // Read event files
 // https://discordjs.guide/creating-your-bot/event-handling.html#individual-event-files
 const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
